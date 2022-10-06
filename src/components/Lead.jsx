204cc02f26b23ef24bf44ba9bca5e88f2bc7d85e
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-10-06 19:19:20 -0400

    feat: add basic markup and styling

--- src/components/Lead.jsx
+++ src/components/Lead.jsx
@@ -1,12 +1,12 @@
-import "./Lead.css";
+import "./Lead.scss";
 const Lead = function (props) {
   return (
     <div className="lead">
       <span className="lead__text">
         Hey! My name is Ryan. I'm passionate about
       </span>
-      <div class="scrolling-words-container">
-        <div class="scrolling-words-box">
+      <div className="scrolling-words-container">
+        <div className="scrolling-words-box">
           <ul>
             <li>web development</li>
             <li>photography</li>
