12e2d8e98386ae21b8644340a7667454f3ed4d94
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-10-06 16:35:06 -0400

    fix: add key to list item

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,9 @@ import SidebarItem from "./SidebarItem";
 import "./Sidebar.css";
 
 function Sidebar({ isOpen, items }) {
-  const renderItems = items.map(item => <SidebarItem item={item} />);
+  const renderItems = items.map(item => (
+    <SidebarItem key={item.title} item={item} />
+  ));
 
   return (
     <ul className={`sidebar ${isOpen ? "open" : "closed"}`}>{renderItems}</ul>
