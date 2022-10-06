bbeedf42479c62b13f8b10aa32e8c6e5fdec88b6
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-10-06 19:15:16 -0400

    chore: use context

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,22 @@
+import { useContext } from "react";
+import { SidebarContext } from "../contexts/SidebarContext";
 import SidebarItem from "./SidebarItem";
 import "./Sidebar.css";
 
-function Sidebar({ isOpen, items }) {
+function Sidebar({ items }) {
+  const { sidebarOpen } = useContext(SidebarContext);
   const renderItems = items.map(item => (
     <SidebarItem key={item.title} item={item} />
   ));
 
   return (
-    <ul className={`sidebar ${isOpen ? "open" : "closed"}`}>{renderItems}</ul>
+    <ul
+      className={`sidebar ${
+        sidebarOpen === null ? "none" : sidebarOpen ? "open" : "closed"
+      }`}
+    >
+      {renderItems}
+    </ul>
   );
 }
 
