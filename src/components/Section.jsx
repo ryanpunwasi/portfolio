2dabed9ed60a88f1cb60140586ad4cae5f5c38c0
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-10-07 17:39:59 -0400

    feat: implement ability to add background image to section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,20 @@
 import styles from "./Section.module.css";
 
 function Section(props) {
+  if (props.image) {
+    return (
+      <div
+        style={{
+          backgroundImage: `url('${props.image}')`,
+          backgroundSize: "100% 100%",
+          backgroundBlendMode: "multiply",
+        }}
+        className={styles.section}
+      >
+        {props.children}
+      </div>
+    );
+  }
   return <div className={styles.section}>{props.children}</div>;
 }
 
