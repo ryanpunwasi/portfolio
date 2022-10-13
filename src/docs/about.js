e0f659ed9896dfcb845e74bb88496befb512aa6d
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-10-13 16:15:07 -0400

    add text for about section

---
+++ src/docs/about.js
@@ -0,0 +1,5 @@
+export const about = `My name is Ryan. I am a full-stack web developer with a passion for creating applications with engaging and intuitive user interfaces. 
+  
+  In my spare time, I enjoy learning about filmmaking and photography, and thinking about football and basketball.
+  
+  To create this website, I used HTML5, CSS3, and React. I learned and implemented CSS transitions, Intersection Observer API, and scroll-based events.`;
