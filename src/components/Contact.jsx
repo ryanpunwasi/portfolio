fd730e6407fd361e64e8ffe052c97b85890aca7a
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-10-27 20:16:27 -0400

    feat: implement turning on lights feature

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,34 +2,49 @@ import "./Contact.scss";
 
 const Contact = ({ lights }) => {
   return (
-    !lights && (
-      <div className="contact__icons">
-        <a
-          href="https://www.linkedin.com/in/ryan-punwasi/"
-          target="_blank"
-          rel="noreferrer"
-          id="linkedin"
-        >
-          <img src="/img/linkedin.png" alt="linkedin" />
-        </a>
-        <a
-          href="https://github.com/ryanpunwasi"
-          target="_blank"
-          rel="noreferrer"
-          id="github"
-        >
-          <img src="/img/github.png" alt="github" />
-        </a>
-        <a
-          href="https:www.twitter.com/RyanPunwasi"
-          target="_blank"
-          rel="noreferrer"
-          id="twitter"
-        >
-          <img src="/img/twitter.png" alt="twitter" />
-        </a>
-      </div>
-    )
+    <>
+      {!lights && (
+        <div className="contact__icons">
+          <span id="linkedin">
+            <img src="/img/linkedin.png" alt="linkedin" />
+          </span>
+          <span id="github">
+            <img src="/img/github.png" alt="github" />
+          </span>
+          <span id="twitter">
+            <img src="/img/twitter.png" alt="twitter" />
+          </span>
+        </div>
+      )}
+      {lights && (
+        <div className="contact__icons">
+          <a
+            href="https://www.linkedin.com/in/ryan-punwasi/"
+            target="_blank"
+            rel="noreferrer"
+            id="linkedin"
+          >
+            <img src="/img/linkedin.png" alt="linkedin" className="light" />
+          </a>
+          <a
+            href="https://github.com/ryanpunwasi"
+            target="_blank"
+            rel="noreferrer"
+            id="github"
+          >
+            <img src="/img/github.png" alt="github" className="light" />
+          </a>
+          <a
+            href="https:www.twitter.com/RyanPunwasi"
+            target="_blank"
+            rel="noreferrer"
+            id="twitter"
+          >
+            <img src="/img/twitter.png" alt="twitter" className="light" />
+          </a>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,15 +1,29 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Contact from "./Contact";
 import "./ContactSection.scss";
 
+import "../";
+
 const ContactSection = () => {
   const [lights, setLights] = useState(false);
+  const audioEl = useRef(null);
+
+  const turnOnLights = () => {
+    if (!lights) {
+      audioEl.current.play();
+      setLights(true);
+    }
+  };
 
   return (
-    <div className="contact__container">
-      <div onClick={() => setLights(!lights)} className="lightswitch">
-        <div className={`switch ${lights ? "on" : ""}`}>Off</div>
-      </div>
+    <div className={`contact__container ${lights ? "on" : "off"}`}>
+      <audio src="/audio/spotlight.mp3" ref={audioEl} preload="auto"></audio>
+      <button
+        onClick={turnOnLights}
+        className={`lightswitch ${lights ? "on" : ""}`}
+      >
+        <i className="fa-solid fa-lightbulb"></i>
+      </button>
       <Contact lights={lights} />
     </div>
   );
