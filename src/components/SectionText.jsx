5331737745fed1e986e7364527d8d1b367138a99
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-10-27 21:23:57 -0400

    edit about section

--- src/components/SectionText.jsx
+++ src/components/SectionText.jsx
@@ -32,21 +32,20 @@ const SectionText = function () {
         Over time, my interest in programming grew and I decided to pivot into
         the software industry. I have experience working on small projects, both
         as an individual and with others. I hope to leverage my insight and
-        skills to join a team and build software that is impactful!
+        skills to build impactful software.
         <br />
         <br />
-        Outside of software, I have two other interests: filmmaking/photography
-        and sports. With regards to filmmaking and photography, I'm always
-        learning about cinematography, video editing, and composition with the
-        goal of producing my own content in the future. When it comes to sports,
-        I'm a big NBA and NFL fan who enjoys learning about the strategic and
-        analytic side of sports.
+        Aside from programming, I'm interested filmmaking/photography and
+        sports. I'm always learning about cinematography, video editing, and
+        composition with the goal of producing my own content in the future.
+        When it comes to sports, I'm a big NBA and NFL fan who enjoys learning
+        about the strategic and analytic side of sports.
         <br />
         <br />
         To create this website, I used HTML5, CSS3, and React. I took the
         opportunity to learn about and implement various web-related features
         including CSS transitions/animations, the Intersection Observer API, and
-        scroll events. I also took the photos! 📸
+        the scroll event. I also took the photos! 📸
       </p>
     </div>
   );
