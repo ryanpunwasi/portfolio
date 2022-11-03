4c90f0b5474536fe0e791f3f36a5ee3f028cf1cb
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-11-02 22:03:39 -0400

    feat: add basic markup and layout for project tab

--- src/components/ProjectTab.jsx
+++ src/components/ProjectTab.jsx
@@ -6,11 +6,33 @@ const ProjectTab = ({ selectedProject, setTab, setSelectedProject }) => {
     setTab("list");
   };
   return (
-    <div>
+    <div className="project__tab">
       <button onClick={back} className="project__back">
-        <i class="fa-solid fa-arrow-left"></i>
+        <i className="fa-solid fa-arrow-left"></i>
       </button>
-      {selectedProject}
+      <div className="project__info">
+        <img src="/img/github.png" />
+        <div>
+          <h6>{selectedProject.name}</h6>
+          <p>{selectedProject.desc}</p>
+
+          <div>
+            <a href={selectedProject.repo} target="_blank" rel="noreferrer">
+              Repo
+            </a>
+            {selectedProject.liveSite && (
+              <a
+                href={selectedProject.liveSite}
+                target="_blank"
+                rel="noreferrer"
+              >
+                Demo
+              </a>
+            )}
+            <span></span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
