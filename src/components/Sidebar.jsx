4110b92c563677834058be7d91d6705e03c313af
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-12-30 21:38:38 -0500

    fix: fix positioning of contact bar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,28 +10,26 @@ function Sidebar({ items }) {
   ));
 
   return (
-    <div className="sidebar-container">
-      <div
-        className={`sidebar ${
-          sidebarOpen === null ? "none" : sidebarOpen ? "open" : "closed"
-        }`}
-      >
-        <ul>{renderItems}</ul>
-      </div>
+    <div
+      className={`sidebar-container ${
+        sidebarOpen === null ? "none" : sidebarOpen ? "open" : "closed"
+      }`}
+    >
+      <ul className="sidebar">{renderItems}</ul>
       <ul className="sidebar-contact">
         <li>
           <i
             style={{ color: "#0072b1" }}
-            className="fa-brands fa-linkedin fa-2xl"
+            className="fa-brands fa-linkedin fa-xl"
           ></i>
         </li>
         <li>
-          <i className="fa-brands fa-square-github fa-2xl"></i>
+          <i className="fa-brands fa-square-github fa-xl"></i>
         </li>
         <li>
           <i
             style={{ color: "#595959" }}
-            className="fa-solid fa-file fa-2xl"
+            className="fa-solid fa-file fa-xl"
           ></i>
         </li>
       </ul>
