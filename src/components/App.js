6dc58e44dd918b96cd1f325bad70484a8b6111d9
Ryan Punwasi <ryan.punwasi@gmail.com>
2023-01-27 20:06:44 -0500

    add semitone api project

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import ScrollTextSection from "./ScrollTextSection";
 import Sidebar from "./Sidebar";
 import Lead from "./Lead";
 import ScrollAnimation from "./ScrollAnimation";
-import Projects from "./Projects";
+import Project from "./Project";
 import Credits from "./Credits";
 import ContactSection from "./ContactSection";
 import "./App.css";
@@ -16,6 +16,77 @@ const sections = [
   { title: "About", link: "#about" },
   { title: "Projects", link: "#projects" },
   { title: "Contact", link: "#contact" },
+];
+
+const projects = [
+  {
+    id: 0,
+    name: "Valentnost",
+    desc: "A single-page web application that helps in the memorization of the names and symbols of the elements of the periodic table",
+    text: "This project was born out of my desire to acquire the obscure skill of being able to match the names and symbols of all the elements of the periodic table. Hopefully high-school students and chemistry enthusiasts can find it useful though! To get this project done, I read up on colour theory, strengthened my understanding of positioning and layout with CSS flexbox and grid, and adhered to test-driven development by writing and using unit tests throughout the development process.",
+    repo: "https://github.com/ryanpunwasi/valentnost",
+    liveSite: "https://valentnost.app",
+    icon: "science.svg",
+    stack: ["HTML", "CSS", "React", "Redux", "Jest"],
+  },
+  {
+    id: 1,
+    name: "Semitone API",
+    desc: "A REST API that serves audio for musical notes in five different octaves",
+    text: "While working on Semitone, I spent a lot of time looking for an simple way to retrieve audio files of musical notes. Unable to find an adequate solution, I decided to create this simple API. I created a schema for musical notes, designed and seeded the database and wrote the endpoints using Express. The homepage was created using EJS templating and Tailwind for styling.",
+    repo: "https://github.com/ryanpunwasi/semitone-api",
+    liveSite: "https://semitone-api.fly.dev",
+    icon: "saxophone.svg",
+    stack: ["HTML5", "TailwindCSS", "Express", "PostgreSQL"],
+  },
+  {
+    id: 2,
+    name: "Semitone",
+    desc: "A single-page web application where users can practice identifying notes, octaves, chords, and scales by sound",
+    text: "I built Semitone with the goal of helping musicians develop their ability to correctly identify pitch. To create this project, I learned about the fundamentals of state management with Redux and how audio is used in the browser. I implemented the layout primarily with Bootstrap, using custom CSS to add my own visual style.",
+    repo: "https://github.com/ryanpunwasi/semitone",
+    liveSite: "https://semitone.io",
+    icon: "piano-2.svg",
+    stack: ["HTML5", "CSS3", "Bootstrap 5", "React", "Redux"],
+  },
+
+  {
+    id: 3,
+    name: "BayStreetBets",
+    desc: "A full-stack web app where users can create and participate in simulated stock trading competitions",
+    text: "BayStreetBets was made with two other collaborators for a final project at Lighthouse Lab's Web Development Bootcamp. I was able to implement authentication and resource authorization with JSON Web Tokens; work on form validation; and implement the functionality to create, delete, leave, and join stock-trading competitions.",
+    repo: "https://github.com/ryanpunwasi/BayStreetBets",
+    liveSite: null,
+    icon: "investment.svg",
+    stack: [
+      "HTML5",
+      "CSS3",
+      "Sass",
+      "Express",
+      "React",
+      "NodeJS",
+      "PostgreSQL",
+    ],
+  },
+  {
+    id: 4,
+    name: "Pinnet",
+    desc: "A full-stack web app where users can create and share maps with pinned locations",
+    repo: "https://github.com/ryanpunwasi/pinnet",
+    text: "Pinnet was made with two other collaborators for a mid-term project at Lighthouse Lab's Web Development Bootcamp. I did work on both the front-end and back-end, implementing features like authentication and the ability to browse, create and favourite maps. I had the chance to write complex SQL queries, create interactive maps with Leaflet, work with promises, and write endpoints with Express.",
+    liveSite: null,
+    icon: "pin.svg",
+    stack: [
+      "HTML5",
+      "Sass",
+      "Bootstrap",
+      "jQuery",
+      "Express",
+      "NodeJS",
+      "EJS",
+      "PostgreSQL",
+    ],
+  },
 ];
 
 function App() {
@@ -90,11 +161,16 @@ function App() {
       <Section>
         <ScrollAnimation image="/img/02.jpeg" id="sunsetImage2" />
       </Section>
-      <Section id="projects">
-        <ScrollTextSection>
-          <h2 className="portfolio-header">Projects</h2>
-        </ScrollTextSection>
-      </Section>
+
+      <ScrollTextSection>
+        <h2 className="portfolio-header">Projects</h2>
+        <Project project={projects[0]} />
+        <Project project={projects[1]} />
+        <Project project={projects[2]} />
+        <Project project={projects[3]} />
+        <Project project={projects[4]} />
+      </ScrollTextSection>
+
       <Section id="contact">
         <ContactSection />
       </Section>
